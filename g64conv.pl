803c5efde0adfc8b1e9c49b021dc2205c47666ab
markusC64 <markusC64@localhost>
2016-03-01 17:22:48 +0100

    First public version

---
+++ g64conv.pl
@@ -0,0 +1,525 @@
+#!/usr/bin/perl
+
+use strict;
+
+if (@ARGV < 2)
+{
+   die "Syntax: g64conv.pl <from> <to> [mode]\n".
+       "supporting .g64 and .txt as file types.\n".
+       "mode may be 0 (hex only) or 1 (gcr parsed, default).\n";
+}
+
+
+my $from = $ARGV[0];
+my $to = $ARGV[1];
+my $level = $ARGV[2];
+$level = 1 unless defined $level;
+
+if ($from =~ /.g64/i)
+{
+   my $g64 = readfileRaw($from);
+   my $txt = g64totxt($g64, $level);
+   writefile($txt, $to);
+}
+else
+{
+   my $txt = readfile($from);
+   my $g64 = txttog64($txt);
+   writefileRaw($g64, $to);
+}
+
+
+
+
+sub readfile
+{
+   my $filename = $_[0];
+   my $file;
+   local $/;
+   undef $/;
+   open($file, "<", $filename);
+   my $ret = <$file>,
+   close $file;
+   $ret;
+}
+
+sub readfileRaw
+{
+   my $filename = $_[0];
+   my $file;
+   local $/;
+   undef $/;
+   open($file, "< :raw", $filename);
+   my $ret = <$file>,
+   close $file;
+   $ret;
+}
+
+sub writefile
+{
+   my ($content, $filename) = @_;
+
+   my $file;
+   open($file, ">", $filename);
+   print $file $content;
+   close $file;
+}
+
+sub writefileRaw
+{
+   my ($content, $filename) = @_;
+
+   my $file;
+   open($file, "> :raw", $filename);
+   print $file $content;
+   close $file;
+}
+
+sub g64totxt
+{
+   my ($g64, $level) = @_;
+   my $ret = "";
+   
+   my $signature = substr($g64, 0, 8);
+   return undef unless $signature eq 'GCR-1541';
+
+   return undef unless substr($g64, 8, 1) eq "\0";
+   
+   my $notracks = unpack("C", substr($g64, 9, 1));
+   my $tracksizeHdr = unpack("S", substr($g64, 0xA, 2));
+   
+   $ret .= "no-tracks $notracks\ntrack-size $tracksizeHdr\n";
+   for (my $i=1; $i<$notracks; $i++)
+   {
+      my $track = ($i+1)/2;
+      my $trackTablePosition = 8+4*$i;
+      my $trackPosition = unpack("L", substr($g64, $trackTablePosition, 4));
+      next unless $trackPosition;
+      my $trackSize = unpack("S", substr($g64, $trackPosition, 2));
+      my $trackContent = substr($g64, $trackPosition+2, $trackSize);
+      
+      my $trackContentHex = unpack("H*", $trackContent);
+      $trackContentHex =~ s/(..)/ $1/gc;
+      
+      my $speedTableOffset = 8+4*$notracks + 4*$i;
+      my $speed = unpack("L", substr($g64, $speedTableOffset, 4));
+      
+      my $trackRet = "track $track\n";
+      if ($level == 0)
+      {
+         $trackRet .= "   speed $speed\n   bytes$trackContentHex\n";
+	 $trackRet .= "end-track\n\n";
+      }
+      else
+      {
+         my $tmp = $trackContentHex;
+	 $tmp =~ s/ //g;
+         my $trackBin = pack("H*", $tmp);
+	 my $trackContentBin = unpack("B*", $trackBin);
+	 
+         $tmp = parseTrack($trackContentBin);
+         $trackRet .= "   speed $speed\n";
+	 unless (defined $tmp)
+	 {
+	    $tmp = "   begin-at 0\n   bytes$trackContentHex\n";
+	    $tmp .= "end-track\n\n";
+	 }
+	 
+	 $trackRet .= $tmp;
+      }
+      
+      $ret .= $trackRet;
+   }
+   
+   
+   $ret;
+}
+
+sub parseTrack
+{
+   my $track = $_[0];
+   
+   my $ret;
+   my $beginat;
+   
+   unless ($track =~ /^(.*?)(1111111111)(.*)$/ )
+   {
+      return undef;
+   }
+
+   $track = "$2$3$1";
+   $beginat = length($1);
+   
+   if ($track =~m/^(.*?)(1{9})(1+)$/)
+   {
+      my $offset = length($3);
+      $track = "$3$1$2";
+      $beginat -= $offset;
+      $beginat += length($track) if $beginat < 0;
+         
+   }
+
+   $ret .= "   begin-at $beginat\n";
+   
+   while ($track ne "")
+   {
+      $track =~ s/^(1{10,})//;
+      $ret .= "   sync " . length($1) . "\n";
+      my $trackPart;
+      my $trackRest;
+      
+      if ($track =~ m/^(.*?1{9})(1.*)$/)
+      {
+         $trackPart = $1;
+	 $trackRest = $2;
+      }
+      else
+      {
+         $trackPart = $track;
+	 $trackRest = "";
+      }
+      
+      $trackPart =~ s/^(.{5})//;
+      my $c = $1;
+      my $a = parseGCR($c);
+      $trackPart =~ s/^(.{5})//;
+      my $d = $1;
+      my $b = parseGCR($d);
+
+      if ($a.$b eq '08')
+      {
+         $ret .= "   ; header\n";
+         $ret .= "   gcr 08\n";
+         for (my $i=0; $i<7; $i++)
+	 {
+            $trackPart =~ s/^(.{5})//;
+	    my $e = $1;
+            my $a = parseGCR($1);
+            $trackPart =~ s/^(.{5})//;
+	    my $f = $1;
+            my $b = parseGCR($1);
+	    
+	    if ($i == 0)
+	    {
+	       $ret .= "   begin-checksum\n";
+	       $ret .= "      checksum $a$b\n" if (defined $a) && (defined $b);
+	       $ret .= "      checksum $e$f\n" unless (defined $a) && (defined $b);
+	    }
+	    else
+	    {
+	       $ret .= "      ; sector\n" if $i == 1;
+	       $ret .= "      ; track\n" if $i == 2;
+	       $ret .= "      ; id2\n" if $i == 3;
+	       $ret .= "      ; id1\n" if $i == 4;
+	       if ((defined $a) && (defined $b))
+	       {
+	          $ret .= "      gcr $a$b\n" if $i < 5 ;
+	       }
+	       else
+	       {
+	          $ret .= "      bits $e$f\n" if $i < 5 ;
+	       }
+	       if ((defined $a) && (defined $b))
+	       {
+	          $ret .= "   gcr $a$b\n" if $i > 4 ;
+	       }
+	       else
+	       {
+	          $ret .= "   bits $e$f\n" if $i > 4 ;
+	       }
+	       $ret .= "   end-checksum\n" if $i == 4;
+	    }
+	 }
+         
+      }
+      elsif ($a.$b eq '07')
+      {
+         $ret .= "   ; data\n";
+         $ret .= "   gcr 07\n";
+
+         $ret .= "   begin-checksum\n";
+	 my $gcr = "";
+         for (my $i=0; $i<259; $i++)
+	 {
+            $trackPart =~ s/^(.{5})//;
+	    my $e = $1;
+            my $a = parseGCR($1);
+            $trackPart =~ s/^(.{5})//;
+	    my $f = $1;
+            my $b = parseGCR($1);
+	    
+	    if ($i < 256)
+	    {
+	       if ((defined $a) && (defined $b))
+	       {
+	          $gcr .= " $a$b";
+	       }
+	       else
+	       {
+	          $ret .= "      gcr$gcr\n" if $gcr;
+	          $ret .= "      bits $e$f\n";
+		  $gcr = "";
+	       }
+	    }
+	    elsif ($i == 256)
+	    {
+	          $ret .= "      gcr$gcr\n" if $gcr;
+	          $ret .= "      checksum $a$b\n" if (defined $a) && (defined $b);
+	          $ret .= "      checksum $e$f\n" unless (defined $a) && (defined $b);
+                 $ret .= "   end-checksum\n";
+	    }
+	    else
+	    {
+	          $ret .= "   gcr $a$b\n" if (defined $a) && (defined $b);
+	          $ret .= "   bits $e$f\n" unless (defined $a) && (defined $b);
+	    }
+	 }
+
+      }
+      else
+      {
+         $ret .= "   gcr $a$b\n" if ((defined $a) && (defined $b));
+	 $ret .= "   bits $c$d\n" unless ((defined $a) && (defined $b));
+      }
+      
+      while (length ($trackPart) >= 8)
+      {
+         $trackPart =~ s/^((.{8})+)//;
+         my $trackBin = pack("B*", $1);
+	 my $trackContentHex = unpack("H*", $trackBin);
+         $trackContentHex =~ s/(..)/ $1/gc;
+	 $ret .= "   bytes$trackContentHex\n";
+
+      }
+      
+      $ret .= "   bits $trackPart\n" if $trackPart ne '';
+      
+      $track = $trackRest;
+      
+      $ret .= "\n";
+   }
+   
+   $ret .= "end-track\n\n";
+   
+   $ret;
+}
+
+
+
+sub parseGCR
+{
+   my $x = $_[0];
+   
+   return '0' if $x eq '01010';
+   return '1' if $x eq '01011';
+   return '2' if $x eq '10010';
+   return '3' if $x eq '10011';
+   return '4' if $x eq '01110';
+   return '5' if $x eq '01111';
+   return '6' if $x eq '10110';
+   return '7' if $x eq '10111';
+   return '8' if $x eq '01001';
+   return '9' if $x eq '11001';
+   return 'a' if $x eq '11010';
+   return 'b' if $x eq '11011';
+   return 'c' if $x eq '01101';
+   return 'd' if $x eq '11101';
+   return 'e' if $x eq '11110';
+   return 'f' if $x eq '10101';
+   undef;
+}
+
+sub nibbleToGCR
+{
+   my $x = $_[0];
+
+   return '01010' if $x eq '0';
+   return '01011' if $x eq '1';
+   return '10010' if $x eq '2';
+   return '10011' if $x eq '3';
+   return '01110' if $x eq '4';
+   return '01111' if $x eq '5';
+   return '10110' if $x eq '6';
+   return '10111' if $x eq '7';
+   return '01001' if $x eq '8';
+   return '11001' if $x eq '9';
+   return '11010' if $x eq 'a';
+   return '11011' if $x eq 'b';
+   return '01101' if $x eq 'c';
+   return '11101' if $x eq 'd';
+   return '11110' if $x eq 'e';
+   return '10101' if $x eq 'f';
+   undef;
+}
+
+
+sub txttog64
+{
+   my $text = $_[0];
+   my $file;
+   my $line;
+   my $tracksizeHdr = 0;
+   my $noTracks = 0;
+   my @tracks = ();
+   my $speed = 4;
+   my $beginat;
+
+   open ($file, "<", \$text);
+   my $curTrack = "";
+   my $curTrackNo = undef;
+   
+   my $checksumBlock = 0;
+   my $checksum = 0;
+   
+   while ($line = <$file>)
+   {
+      chomp $line;
+      $line =~s/^ +//;
+      
+      if ($line eq "")
+      {
+      }
+      elsif ($line =~ /^;/)
+      {
+      }
+      elsif ($line =~ /^no-tracks (.*)$/)
+      {
+         $noTracks = $1;
+      }
+      elsif ($line =~ /^track-size (.*)$/)
+      {
+         $tracksizeHdr = $1;
+      }
+      elsif ($line =~ /^track (.*)$/)
+      {
+	 $curTrackNo = $1*2-1;
+	 $curTrack = "";
+	 $beginat = 0;
+	 $checksumBlock = 0;
+      }
+      elsif ($line eq "end-track")
+      {
+         my $len = length($curTrack);
+	 die "Track length $len bits is not a multilpe of 8 bits\n" if $len % 8;
+	 
+         if ($curTrackNo)
+	 {
+	    $tracks[$curTrackNo] = [ $speed, $curTrack ];
+	 }
+         $checksumBlock = 0;
+      }
+      elsif ($line =~ /^speed (.*)$/)
+      {
+         $speed = $1;
+      }
+      elsif ($line =~ /^begin-at (.*)$/)
+      {
+         $beginat = $1;
+      }
+      elsif ($line =~ /^sync (.*)$/)
+      {
+         my $par = $1;
+	 $curTrack .= 1 x $par;
+	 $checksumBlock = 2 if $checksumBlock == 1;
+      }
+      elsif ($line =~ /^bits (.*)$/)
+      {
+         my $par = $1;
+	 $par =~ s/ //g;
+	 $curTrack .= $par;
+	 $checksumBlock = 2 if $checksumBlock == 1;
+      }
+      elsif ($line =~ /^bytes (.*)$/)
+      {
+         my $par = $1;
+	 $par =~ s/ //g;
+         my $trackBin = pack("H*", $par);
+	 my $trackContentBin = unpack("B*", $trackBin);
+	 $curTrack .= $trackContentBin;
+	 $checksumBlock = 2 if $checksumBlock == 1;
+      }
+      elsif ($line eq 'begin-checksum')
+      {
+         $checksumBlock = 1;
+	 $checksum = 0;
+      }
+      elsif ($line eq 'end-checksum')
+      {
+         if ($checksumBlock == 1)
+	 {
+	    my $tmp = unpack("H*", chr($checksum));
+	    my $tmp2 = nibbleToGCR( substr($tmp, 0, 1) ) . nibbleToGCR( substr($tmp, 1, 1) );
+	    $curTrack =~ s/-{10}/$tmp/g;
+	 }
+	 $checksumBlock = 0;
+      }
+      elsif ($line =~ /^gcr (.*)$/)
+      {
+         my $par = $1;
+	 $par =~ s/ //g;
+	 
+	 for my $i (split //, $par)
+	 {
+	    $curTrack .= nibbleToGCR($i);
+	 }
+	 
+	 if ($checksumBlock == 1)
+	 {
+            my $tmp = pack("H*", $par);
+	    my @tmp = unpack("C*", $tmp);
+	    for my $i (@tmp)
+	    {
+	       $checksum ^= $i;
+	    }
+	 }
+      }
+      elsif ($line =~ /^checksum(.*)$/)
+      {
+         my $par = $1;
+	 $par =~ s/ //g;
+	 
+	 if ($par ne '')
+	 {
+	    for my $i (split //, $par)
+	    {
+	       $curTrack .= nibbleToGCR($i);
+	    }
+	 }
+	 else
+	 {
+	    $curTrack .= "-" x 10;
+	 }
+      }
+      else
+      {
+         print "Unknown line: $line\n";
+      }
+   }
+   close $file;
+   
+   my $g64 = "GCR-1541\0" . pack("C", $noTracks) . pack("S", $tracksizeHdr);
+   $g64 .= "\0\0\0\0" x $noTracks;
+   $g64 .= "\0\0\0\0" x $noTracks;
+   
+   for (my $i=1; $i<$noTracks; $i++)
+   {
+      next unless defined $tracks[$i];
+      my $trackSpeed = $tracks[$i]->[0]-0;
+      my $trackContent = $tracks[$i]->[1];
+      
+      my $track2 = ($i+1)/2;
+      my $trackTablePosition = 8+4*$i;
+      my $speedTableOffset = 8+4*$noTracks + 4*$i;
+      
+      my $tmp = pack("L", length($g64));
+      substr($g64, $trackTablePosition, 4) = $tmp;
+      substr($g64, $speedTableOffset, 4) = pack("L", $trackSpeed);
+      
+      my $tmp = pack("B*", $trackContent);
+      my $siz = length($tmp);
+      my $tmpSize = pack("S", $siz);
+      $g64 .= $tmpSize.$tmp."\0\0";
+   }
+   
+   $g64;
+}
